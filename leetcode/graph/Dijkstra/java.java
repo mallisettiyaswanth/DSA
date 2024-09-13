40d1caf5b1eaff29a7aafbbc9dd93a69f22da91b
Yaswanth791504 <mallisettiyaswanth@gmail.com>
2024-09-13 11:23:03 +0530

    13-9-24/1

--- leetcode/graph/Dijkstra/java.java
+++ leetcode/graph/Dijkstra/java.java
@@ -1,37 +1,3 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
-class DriverClass
-{
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String str[] = read.readLine().trim().split(" ");
-            int V = Integer.parseInt(str[0]);
-            int E = Integer.parseInt(str[1]);
-
-            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
-            for(int i=0;i<V;i++)
-            {
-                adj.add(new ArrayList<ArrayList<Integer>>());
-            }
-
-            int i=0;
-            while (i++<E) {
-                String S[] = read.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                int w = Integer.parseInt(S[2]);
-                ArrayList<Integer> t1 = new ArrayList<Integer>();
-                ArrayList<Integer> t2 = new ArrayList<Integer>();
-                t1.add(v);
-                t1.add(w);
-                t2.add(u);
 class Solution {
     class Node {
         int dis;
