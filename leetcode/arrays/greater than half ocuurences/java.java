654a426278a1b7dce39ac740078cac1a2f99dbe6
yaswanth-2005 <mallisettiyaswanth@gmail.com>
2024-11-23 21:18:10 +0530

    23-11-2024/3

--- leetcode/arrays/greater than half ocuurences/java.java
+++ leetcode/arrays/greater than half ocuurences/java.java
@@ -15,3 +15,20 @@ class Solution {
 
     }
 }
+
+import java.util.*;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int ele = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if(count == 0) {
+                ele = nums[i];
+            }
+            if(ele == nums[i]) count++;
+            else count--;
+        }
+        return ele;
+    }
+}
